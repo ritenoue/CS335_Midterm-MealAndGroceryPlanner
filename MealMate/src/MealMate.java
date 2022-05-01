c1d45e3e694bd437df7a2888137a5f17d5275079
ritenoue <ritenour.ej@gmail.com>
2022-05-01 13:59:56 -0400

    indentation error fix'

--- MealMate/src/MealMate.java
+++ MealMate/src/MealMate.java
@@ -279,7 +279,7 @@ public class MealMate extends JFrame implements ActionListener {
 				p.write(item + "\r\n");
 				// Window to confirm
 				JFrame jFrame = new JFrame();
-        		J	OptionPane.showMessageDialog(jFrame, "Successfully added '"+item+"' to your pantry.");
+        			JOptionPane.showMessageDialog(jFrame, "Successfully added '"+item+"' to your pantry.");
 				// set the text of field to blank
 				t.setText("");
 			}
